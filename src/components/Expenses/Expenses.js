8371cb776d9419b9931faf5840f7afd5c7aece47
Tomasz Szkonter <Tomasz.Szkonter@gmail.com>
2021-05-21 21:17:53 +0200

    v0.2
    Added expenses chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,48 +1,29 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "../Expenses/ExpenseItem";
+import ExpensesList from "../Expenses/ExpensesList";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 import Card from "../UI/Card";
 import "../Expenses/Expenses.css";
 
-const Expenses = props => {
-    const [selectedYear, setSelectedYear] = useState('2020');
+const Expenses = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2020");
 
-    const yearHandler = (selectedYear) => {
-        setSelectedYear(selectedYear.year);
-    };
+  const yearHandler = (selectedYear) => {
+    setSelectedYear(selectedYear.year);
+  };
 
-    console.log(selectedYear);
-
-    return (
-        <Card className="expenses">
-            <ExpensesFilter orginSelect={selectedYear} onYearPicker={yearHandler} />
-            <ExpenseItem
-                key={props.items[0].id}
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}
-            />
-            <ExpenseItem
-                key={props.items[1].id}
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date}
-            />
-            <ExpenseItem
-                key={props.items[2].id}
-                title={props.items[2].title}
-                amount={props.items[2].amount}
-                date={props.items[2].date}
-            />
-            <ExpenseItem
-                key={props.items[3].id}
-                title={props.items[3].title}
-                amount={props.items[3].amount}
-                date={props.items[3].date}
-            />
-        </Card>
-    )
-}
+  const filteredExpense = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === selectedYear;
+  });
+  
+  return (
+    <Card className="expenses">
+      <ExpensesFilter orginSelect={selectedYear} onYearPicker={yearHandler} />
+      <ExpensesChart expenses={filteredExpense} />
+      <ExpensesList items={filteredExpense} />
+    </Card>
+  );
+};
 
 export default Expenses;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
-    const saveExpenseDataHandler = (enteredExpenseData) => {
-        const expenseData = {
-            ...enteredExpenseData,
-            id: Math.random().toString()
-        };
-        props.onAddExpense(expenseData);
+  const [formVisib, setFormVisib] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
     };
+    props.onAddExpense(expenseData);
+  };
 
+  const showForm = (event) => {
+    event.preventDefault();
+    return formVisib ? setFormVisib(false) : setFormVisib(true);
+  };
 
+  if (formVisib === false) {
     return (
-        <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-        </div>
-    )
-}
+      <div className="new-expense">
+        <button onClick={showForm}>Add New Expense</button>{" "}
+      </div>
+    );
+  }
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm
+        onSaveExpenseData={saveExpenseDataHandler}
+        onVisibChange={showForm}
+      />
+    </div>
+  );
+};
 
 export default NewExpense;
